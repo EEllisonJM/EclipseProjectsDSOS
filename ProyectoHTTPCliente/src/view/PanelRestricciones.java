594c364aac174f3d441de271ed6c74e5d34bb0bc
EEllisonJM <eellison_jm@homail.com>
2018-05-08 13:09:00 -0500

    PanelRestricciones avance mejorado

--- ProyectoHTTPCliente/src/view/PanelRestricciones.java
+++ ProyectoHTTPCliente/src/view/PanelRestricciones.java
@@ -1,6 +1,11 @@
 
 package view;
 
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.util.List;
+
+import javax.swing.BorderFactory;
 import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -8,74 +13,67 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 public class PanelRestricciones extends JPanel implements ChangeListener {
+	private String[] contenidos = new String[] { //
+			"Mayor que", "Menor que", // 0[>], 1[<]
+			"Igual a", "Diferente de", // 2[==], 3[!=]
+			"Mayor o igual a", "Menor o Igual a" };// 4[>=], 5[<=]
 
-	private JCheckBox jcheckMayor,jcheckMenor,jcheckIgual,jcheckMulti;
-	private JTextField jtextMayor,jtextMenor,jtextIgual,jtextMulti;
-	private String[][] restricciones = {{"Mayor que",""},{"Menor que",""},{"Exactamente",""},{"Multiplo de",""}};
-	
+	private JCheckBox[] jChecksDesIgualdades;
+	private JTextField[] jTextFields;
+
+	private List<Object[]> listaRestricciones;// [String][Integer]
+
 	public PanelRestricciones() {
-		
-		setLayout(null);
-		jcheckMayor=new JCheckBox("Mayor que");
-		jcheckMenor=new JCheckBox("Menor que");
-		jcheckIgual=new JCheckBox("Exactamente");
-		jcheckMulti=new JCheckBox("Multiplo de ");
-		jtextMayor=new JTextField("");
-		jtextMenor=new JTextField("");
-		jtextIgual=new JTextField("");
-		jtextMulti=new JTextField("");
-		
-        jcheckMayor.setBounds(10,10,120,30);
-        jcheckMenor.setBounds(10,40,120,30);
-        jcheckIgual.setBounds(10,70,120,30);
-        jcheckMulti.setBounds(10,100,120,30);
-        jtextMayor.setBounds(130,10,50,30);
-		jtextMenor.setBounds(130,40,50,30);
-		jtextIgual.setBounds(130,70,50,30);
-		jtextMulti.setBounds(130,100,50,30);
-        
-        jcheckMayor.addChangeListener(this);
-        jcheckMenor.addChangeListener(this);
-        jcheckIgual.addChangeListener(this);
-        jcheckMulti.addChangeListener(this);
+		this.setSize(new Dimension(30, 30));
+		this.setBorder(BorderFactory.createTitledBorder("Realizar restricciones"));
+		this.setToolTipText("Realizar restricciones (>, <, =, !=, >=, <=");
+		this.setLayout(new GridLayout(0, 2));
 
-        add(jcheckMayor);
-        add(jcheckMenor);
-        add(jcheckIgual);
-        add(jcheckMulti);
-        add(jtextMayor);
-        add(jtextMenor);
-        add(jtextIgual);
-        add(jtextMulti);
+		jChecksDesIgualdades = new JCheckBox[contenidos.length];
+		jTextFields = new JTextField[contenidos.length];
+		inicializarJCheckBoxs(jChecksDesIgualdades);
+		inicializarCamposDeTexto(jTextFields);
+		agrearComponente(jChecksDesIgualdades, jTextFields);
 	}
-	
-	public String[][] getRestricciones() {
-		return restricciones;
+
+	public void inicializarJCheckBoxs(JCheckBox jCheckBoxs[]) {
+		for (int i = 0; i < jCheckBoxs.length; i++) {
+			jCheckBoxs[i] = new JCheckBox(contenidos[i]);
+			jCheckBoxs[i].addChangeListener(this);
+
+		}
 	}
-	
+
+	public void inicializarCamposDeTexto(JTextField jTextFields[]) {
+		for (int i = 0; i < jTextFields.length; i++) {
+			jTextFields[i] = new JTextField(2);
+		}
+	}
+
+	public void agrearComponente(JCheckBox[] jCheckBoxs, JTextField[] jTextFields) {
+		for (int i = 0; i < jTextFields.length; i++) {
+			this.add(jCheckBoxs[i]);
+			this.add(jTextFields[i]);
+		}
+	}
+
+	public List<Object[]> getListaRestricciones() {
+		for (int i = 0; i < jChecksDesIgualdades.length; i++) {
+			if (jChecksDesIgualdades[i].isSelected()) {
+				listaRestricciones.add(new Object[] { contenidos[i], jTextFields[i] });
+				System.out.println(contenidos[i]);
+				System.out.println(jTextFields[i]);
+			}
+		}
+		return listaRestricciones;
+	}
+
 	@Override
 	public void stateChanged(ChangeEvent e) {
 		// TODO Auto-generated method stub
-        if (jcheckMayor.isSelected()==true) {
-            restricciones[0][1]=jtextMayor.getText();
-        } else {
-        	restricciones[0][1]="";
-        }
-        if (jcheckMenor.isSelected()==true) {
-        	restricciones[1][1]=jtextMenor.getText();
-        } else {
-        	restricciones[1][1]="";
-        }
-        if (jcheckIgual.isSelected()==true) {
-        	restricciones[2][1]=jtextIgual.getText();
-        } else {
-        	restricciones[2][1]="";
-        }
-        if (jcheckMulti.isSelected()==true) {
-        	restricciones[3][1]=jtextMulti.getText();
-        } else {
-        	restricciones[3][1]="";
-        }
-	} 
-	
+		/*
+		 * if (jChecksDesIgualdades[0].isSelected() == true) {
+		 * System.out.println(jChecksDesIgualdades[0].getText()); }
+		 */
+	}
 }
