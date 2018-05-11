fb65845804b5a5a76086d7510be3f98a1378749d
EEllisonJM <eellison_jm@homail.com>
2018-05-10 20:02:03 -0500

    Manejo de Datos

--- ProyectoHTTPCliente/src/view/PanelTabla.java
+++ ProyectoHTTPCliente/src/view/PanelTabla.java
@@ -1,8 +1,11 @@
 package view;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import controller.CSVHandler;
 import model.Numero;
@@ -13,14 +16,33 @@ public class PanelTabla extends JPanel {
 	private Object[][] datos;
 	private String[] encabezados;
 
+	/* Panel tabla procesado */
+	public PanelTabla(String host, List<Object[]> restriccionesPlus) throws IOException {
+		/* Obtener Lista de Numeros */
+		CSVHandler csvHandler = new CSVHandler(',');
+		List<Numero[]> numeros = csvHandler.parsearArchivoNumerosRestricciones("archivos/" + host + ".csv",
+				restriccionesPlus);
+		/* Crear Tabla con valores y encabezado */
+		tabla = new JTable(parsearListaNumeros(numeros), getEncabezado());
+		JScrollPane jScrollBar = new JScrollPane(tabla, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		this.add(jScrollBar);
+
+		this.validate();
+		this.repaint();
+	}
+
+	/* Panel tabla original */
 	public PanelTabla(String host) throws IOException {
 		/* Obtener Lista de Numeros */
 		CSVHandler csvHandler = new CSVHandler(',');
-		List<Numero[]> numeros = csvHandler.leerArchivoNumeros("archivos/" + host + ".csv");
+		List<Numero[]> numeros = csvHandler.parsearArchivoNumeros("archivos/" + host + ".csv");
 		/* Crear Tabla con valores y encabezado */
 		tabla = new JTable(parsearListaNumeros(numeros), getEncabezado());
-		this.add(tabla);
-		
+		JScrollPane jScrollBar = new JScrollPane(tabla, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		this.add(jScrollBar);
+
 		this.validate();
 		this.repaint();
 	}
@@ -45,7 +67,10 @@ public class PanelTabla extends JPanel {
 		datos = new Object[numeros.size()][maxColumnas];
 		for (int i = 0; i < numeros.size(); i++) {
 			for (int j = 0; j < numeros.get(i).length; j++) {
-				datos[i][j] = numeros.get(i)[j].getValor();
+				if (numeros.get(i)[j] != null) {
+					datos[i][j] = numeros.get(i)[j].getValor();
+					System.out.println(numeros.get(i)[j].getValor());
+				}
 			}
 		}
 		return datos;
