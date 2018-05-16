619755a5865b55e513aef393e63a73c0de2e0383
EEllisonJM <eellison_jm@homail.com>
2018-05-16 17:44:21 -0500

    Validacion campos vacios

--- ProyectoHTTPCliente/src/view/GUI_Principal.java
+++ ProyectoHTTPCliente/src/view/GUI_Principal.java
@@ -5,11 +5,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
 public class GUI_Principal extends JFrame implements ActionListener {
-
 	private JTabbedPane tablaPaneles;
 	private JPanel panelPrincipal;
 	private PanelNodos panelNodos;
@@ -47,11 +47,17 @@ public class GUI_Principal extends JFrame implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		PanelResultados panelResultados = new PanelResultados(tablaPaneles, panelNodos.getJtfRutaCSV(),
-				panelNodos.getJtfNombreArchivo(), Integer.parseInt(panelNodos.getJtfPuerto()));
+		if (panelNodos.getJtfRutaCSV().equals("") || panelNodos.getJtfNombreArchivo().equals("")
+				|| panelNodos.getJtfPuerto().equals("")) {
+			JOptionPane.showMessageDialog(null, "Favor de llenar los campos.");
+		} else {
+
+			new PanelResultados(tablaPaneles, panelNodos.getJtfRutaCSV(), panelNodos.getJtfNombreArchivo(),
+					Integer.parseInt(panelNodos.getJtfPuerto()));
+		}
 	}
 
 	public static void main(String[] args) {
-		GUI_Principal gui_Principal = new GUI_Principal();
+		new GUI_Principal();
 	}
 }
